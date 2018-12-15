89a190480d4c198bc92ab1afd6bf13f29ca26b14
Stephen Yao <stephenryao@gmail.com>
2018-12-15 16:06:36 +1100

    Added code to do oauth login to REA github enterprise and return an access token

--- midnight-cat-mac/AppDelegate.swift
+++ midnight-cat-mac/AppDelegate.swift
@@ -11,16 +11,31 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+    let clientID = "8a4b44c3d84d750edbc7"
+    let loginUrl = URL(string: "https://git.realestate.com.au/login/oauth/authorize?scope=user&client_id=\(clientID)")!
+    NSWorkspace.shared.open(loginUrl)
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
   }
 
+  func application(_ application: NSApplication, open urls: [URL]) {
+    guard let url = urls.first else {
+      return
+    }
+    
+    let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+    guard
+      let queryItem = components?.queryItems?.first(where: { $0.name == "access_token" }),
+      let accessToken = queryItem.value
+    else {
+      return
+    }
+    
+    print("access token retrieved: \(accessToken)")
+  }
 
 }
 
