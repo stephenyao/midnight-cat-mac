8c9df99f83c8fae52166452bd29b6aa90114d3e0
Stephen Yao <stephenryao@gmail.com>
2018-12-15 12:54:21 +1100

    Initial Commit

---
+++ midnight-cat-mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  midnight-cat-mac
+//
+//  Created by Stephen Yao on 15/12/18.
+//  Copyright © 2018 REA-Group. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
