e1f4c350f8db077b7a185b6f2800e5494dca201d
Stephen Yao <stephenryao@gmail.com>
2018-12-27 18:06:34 +1100

    Added test bundle

---
+++ midnight-cat-macTests/midnight_cat_macTests.swift
@@ -0,0 +1,33 @@
+//
+//  midnight_cat_macTests.swift
+//  midnight-cat-macTests
+//
+//  Created by Stephen Yao on 27/12/18.
+//  Copyright © 2018 REA-Group. All rights reserved.
+//
+
+import XCTest
+
+class midnight_cat_macTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
