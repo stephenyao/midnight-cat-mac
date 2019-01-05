36edeb8250c6b0a8466b66c8c7e2eb416f2da6c1
Stephen Yao <stephenryao@gmail.com>
2019-01-05 11:25:20 +1100

    Added tableview delegate

--- midnight-cat-mac/Onboarding/RepositoriesSelectViewController.swift
+++ midnight-cat-mac/Onboarding/RepositoriesSelectViewController.swift
@@ -17,8 +17,20 @@ struct RepositoriesSelectViewModel {
   }
 }
 
-class RepositoriesSelectViewController: NSViewController {
+class RepositoriesSelectViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
   
   var viewModel: RepositoriesSelectViewModel!
+  
+  func numberOfRows(in tableView: NSTableView) -> Int {
+    return self.viewModel.sectionData[0].count
+  }
+  
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+    let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cellId"), owner: nil) as? NSTableCellView
+    
+    cell?.textField?.stringValue = self.viewModel.sectionData[0][row].name ?? ""
+    
+    return cell
+  }
   
 }
