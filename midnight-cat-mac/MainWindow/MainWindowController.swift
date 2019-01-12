dea282e58b01e90257302cc608855bfbffc4466f
stephen yao <stephen.yao@rea-group.com>
2019-01-13 09:04:44 +1100

    Split out the main window presenter to separate file

--- midnight-cat-mac/MainWindow/MainWindowController.swift
+++ midnight-cat-mac/MainWindow/MainWindowController.swift
@@ -8,74 +8,6 @@
 
 import Cocoa
 
-private protocol MainWindowPresenterDelegate: class {
-  
-  func presentZeroState()
-  
-  func presentRepositories()
-  
-  func presentLoggedOut()
-  
-}
-
-private final class MainWindowPresenter: DataStoreObserver {
-  
-  private let dataStore: DataStore
-  
-  weak var delegate: MainWindowPresenterDelegate? {
-    didSet {
-      let objects: [GitRepository] = self.dataStore.objects(with: "repositories")
-      
-      if !AppState.sharedInstance.isSignedIn {
-        self.delegate?.presentLoggedOut()
-      } else {
-        if objects.count == 0 {
-          self.delegate?.presentZeroState()
-        } else {
-          self.delegate?.presentRepositories()
-        }
-      }
-    }
-  }
-  
-  init(dataStore: DataStore = Database.sharedInstance) {
-    self.dataStore = dataStore
-    self.dataStore.add(observer: self)
-    NotificationCenter.default.addObserver(self, selector: #selector(onSignInSuccess), name: AppNotifications.signInSuccess, object: nil)
-  }
-  
-  deinit {
-    self.dataStore.remove(observer: self)
-    NotificationCenter.default.removeObserver(self)
-  }
-  
-  var uniqueObserverKey: String {
-    return "MainWindowDataObserver"
-  }
-  
-  var collectionName: String {
-    return "repositories"
-  }
-  
-  func onCollectionChanged(objects: [Storable]) {
-    if objects.count == 0 {
-      self.delegate?.presentZeroState()
-    } else {
-      self.delegate?.presentRepositories()
-    }
-  }
-  
-  @objc private func onSignInSuccess() {
-    let objects: [GitRepository] = self.dataStore.objects(with: "repositories")
-    if objects.count == 0 {
-      self.delegate?.presentZeroState()
-    } else {
-      self.delegate?.presentRepositories()
-    }
-  }
-  
-}
-
 class MainWindowController: NSWindowController, MainWindowPresenterDelegate {
   
   private let presenter: MainWindowPresenter = MainWindowPresenter(dataStore: Database.sharedInstance)
@@ -89,17 +21,17 @@ class MainWindowController: NSWindowController, MainWindowPresenterDelegate {
     presentZeroState()
   }
   
-  fileprivate func presentRepositories() {
+  func presentRepositories() {
     let repositoryContentViewController = RepositoryContentViewController.init(nibName: nil, bundle: nil)
     self.window?.contentViewController = repositoryContentViewController
   }
   
-  fileprivate func presentZeroState() {
+  func presentZeroState() {
     let zeroStateViewController = ZeroStateViewController.init(nibName: nil, bundle: nil)
     self.window?.contentViewController = zeroStateViewController
   }
   
-  fileprivate func presentLoggedOut() {
+  func presentLoggedOut() {
     let loggedOutViewController = LoggedOutViewController.init(nibName: nil, bundle: nil)
     self.window?.contentViewController = loggedOutViewController
   }
---
+++ midnight-cat-mac/MainWindow/MainWindowPresenter.swift
@@ -0,0 +1,77 @@
+//
+//  MainWindowPresenter.swift
+//  midnight-cat-mac
+//
+//  Created by Stephen Yao on 13/1/19.
+//  Copyright © 2019 REA-Group. All rights reserved.
+//
+
+import Foundation
+
+protocol MainWindowPresenterDelegate: class {
+  
+  func presentZeroState()
+  
+  func presentRepositories()
+  
+  func presentLoggedOut()
+  
+}
+
+final class MainWindowPresenter: DataStoreObserver {
+  
+  private let dataStore: DataStore
+  
+  weak var delegate: MainWindowPresenterDelegate? {
+    didSet {
+      let objects: [GitRepository] = self.dataStore.objects(with: "repositories")
+      
+      if !AppState.sharedInstance.isSignedIn {
+        self.delegate?.presentLoggedOut()
+      } else {
+        if objects.count == 0 {
+          self.delegate?.presentZeroState()
+        } else {
+          self.delegate?.presentRepositories()
+        }
+      }
+    }
+  }
+  
+  init(dataStore: DataStore = Database.sharedInstance) {
+    self.dataStore = dataStore
+    self.dataStore.add(observer: self)
+    NotificationCenter.default.addObserver(self, selector: #selector(onSignInSuccess), name: AppNotifications.signInSuccess, object: nil)
+  }
+  
+  deinit {
+    self.dataStore.remove(observer: self)
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  var uniqueObserverKey: String {
+    return "MainWindowDataObserver"
+  }
+  
+  var collectionName: String {
+    return "repositories"
+  }
+  
+  func onCollectionChanged(objects: [Storable]) {
+    if objects.count == 0 {
+      self.delegate?.presentZeroState()
+    } else {
+      self.delegate?.presentRepositories()
+    }
+  }
+  
+  @objc private func onSignInSuccess() {
+    let objects: [GitRepository] = self.dataStore.objects(with: "repositories")
+    if objects.count == 0 {
+      self.delegate?.presentZeroState()
+    } else {
+      self.delegate?.presentRepositories()
+    }
+  }
+  
+}
